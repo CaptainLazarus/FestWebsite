30e5dbf7bab81529cf78dc0f3aef80f5d65d24a2
MisterKnight <adityagudimetla@gmail.com>
2020-02-04 03:31:54 +0530

    Added More Pages

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
-const target = window.document.getElementsByClassName('neon')[0]
+const flickerLetter = letter => 
+  `<span style="animation: text-flicker-in-glow ${Math.random()*4}s linear both ">
+    ${letter}
+  </span>`
+const colorLetter = letter => 
+  `<span style="color: hsla(${Math.random()*360}, 100%, 80%, 1);">${letter}</span>`;
+const flickerAndColorText = text => 
+  text.split('').map(flickerLetter).map(colorLetter).join('');
+const neonGlory = t => 
+  t.innerHTML = flickerAndColorText(t.textContent);
+const targets = window.document.getElementsByClassName('neon')
+console.log(targets)
 
-const flickerLetter = letter => `<span style="animation: text-flicker-in-glow ${Math.random()*4}s linear both ">${letter}</span>`
-const colorLetter = letter => `<span style="color: hsla(${Math.random()*360}, 100%, 80%, 1);">${letter}</span>`;
 
-const flickerAndColorText = text => text.split('').map(flickerLetter).map(colorLetter).join('');
-const neonGlory = target => target.innerHTML = flickerAndColorText(target.textContent);
+for(var i=0 ; i<targets.length ; i++){
+  neonGlory(targets[i]);
+  targets[i].onclick = ({ target }) =>  neonGlory(target);
+}
 
-neonGlory(target);
-target.onclick = ({ target }) =>  neonGlory(target);
+$(window).scroll(function(e){
+  parallax();
+});
+
+function parallax(){
+  var scrolled = $(window).scrollTop();
+  $('.background').css('left',-(scrolled*0.15)+'px');
+}
